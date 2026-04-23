97135e7f7a95db0e9cbe8118b16763cb42ade9d2
Jonathan Bown <Jonathan.k.bown@gmail.com>
2026-04-23 09:06:57 -0400

    update experiment 1, 2

---
+++ training/agents/experiment1/scorers_1.py
@@ -0,0 +1,16 @@
+from mlflow.genai.scorers import (
+Correctness,
+ExpectationsGuidelines,
+Safety,
+RelevanceToQuery,
+Guidelines,
+)
+
+JUDGE_MODEL="gemini:/gemini-3.1-flash-lite-preview"
+
+MLFLOW_SCORERS = [
+    Correctness(model=JUDGE_MODEL),
+    RelevanceToQuery(model=JUDGE_MODEL),
+    Safety(model=JUDGE_MODEL),
+    ExpectationsGuidelines(model=JUDGE_MODEL),
+]
